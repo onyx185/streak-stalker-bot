c236665e3ec1016e56434542e2b131d6cfb94db0
Prem Kumar <premkumarmvks@gmail.com>
2023-10-12 23:55:11 +0530

    Alert messages implemented, with quotes.

---
+++ src/cogs/Alerts.py
@@ -0,0 +1,60 @@
+from discord.ext import commands, tasks
+import discord
+from src.database.user_alerts import UserAlerts, get_motivational_quote
+from datetime import datetime, timedelta, timezone
+from src.constant import REMINDER_TIME_FOR_ALERT_IN_24H_FORMAT
+
+
+class Alerts(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.check_user_last_post_and_send_alert.start()
+
+    def cog_unload(self) -> None:
+        self.check_user_last_post_and_send_alert.stop()
+
+    @tasks.loop(seconds=5)
+    async def check_user_last_post_and_send_alert(self):
+        current_time = datetime.now(tz=timezone(timedelta(hours=5, minutes=30))).strftime('%H')
+
+        # if current_time in REMINDER_TIME_FOR_ALERT_IN_24H_FORMAT:
+        if True:
+            user_details = UserAlerts().get_not_posted_users_details()
+
+            for user_id, challenge_name in zip(user_details['user_id'], user_details['challenge_name']):
+                try:
+                    user = await self.client.fetch_user(user_id)
+
+                    print("Posted message")
+
+                    quote = get_motivational_quote()
+
+                    std_color = discord.Colour.red()
+
+                    embed_quotes = discord.Embed(
+                        title=f"{quote['content']}",
+                        color=std_color
+                    )
+
+                    embed_quotes.set_author(name=f"{quote['author']}")
+
+                    embed_message = discord.Embed(
+                        title=f"Challenge update remainder 🕔",
+                        color=std_color
+                    )
+
+                    embed_message.add_field(
+                        name=f"{user.display_name}",
+                        value=f"\nYou have not posted an update for **{challenge_name}** for the day.👨‍💻\n"
+                              f"Please update to maintain the streak 🏆",
+                        inline=False
+                    )
+
+                    await user.send("", embeds=[embed_quotes, embed_message])
+
+                except Exception as e:
+                    print(e)
+
+
+async def setup(client):
+    await client.add_cog(Alerts(client))
---
+++ src/database/user_alerts.py
@@ -0,0 +1,58 @@
+from src.database.database import UsersChallenges_collection, challenges_collection
+from datetime import datetime, timedelta, timezone
+import requests
+
+
+class UserAlerts:
+    def __init__(self):
+        self.today = datetime.now(tz=timezone(timedelta(hours=5, minutes=30)))
+
+    def get_not_posted_users_details(self) -> dict:
+
+        query = {'last_posted_date': {
+            '$lt': self.today
+        }}
+
+        results = UsersChallenges_collection.find(query)
+
+        user_ids_and_challenge_ids = {'user_id': [], 'challenge_id': [], 'challenge_name': []}
+
+        for result in results:
+            user_ids_and_challenge_ids['user_id'].append(result['user_id'])
+            user_ids_and_challenge_ids['challenge_id'].append(result['challenge_id'])
+
+        challenge_ids = user_ids_and_challenge_ids['challenge_id']
+        challenge_names = self._get_challenges_name(challenge_ids=challenge_ids)
+
+        user_ids_and_challenge_ids['challenge_name'] = challenge_names
+
+        return user_ids_and_challenge_ids
+
+    def _get_challenges_name(self, challenge_ids: list) -> list:
+
+        query = {
+            'challenge_id': {
+                '$in': challenge_ids
+            }
+        }
+        results = challenges_collection.find(query)
+        challenge_names = []
+
+        for result in results:
+            challenge_names.append(result['challenge_name'])
+
+        return challenge_names
+
+
+def get_motivational_quote():
+    url = "https://api.quotable.io/random?tags=knowledge|education|change|inspiration|discipline|consistency"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        quote = {'content': data['content'], 'author': data['author']}
+
+        return quote
+    else:
+        return "Failed to fetch a quote."
